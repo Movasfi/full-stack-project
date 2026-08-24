64bf7b52d106d2094458df87a6c7907b05557564
Movasfi <vvasva860@gmail.com>
2026-08-24 10:26:05 +0200

    added login and logout and made crud operation

---
+++ backend/app/Http/Controllers/TasksController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTasksRequest;
+use App\Http\Requests\UpdateTasksRequest;
+use App\Models\Tasks;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTasksRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tasks $tasks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTasksRequest $request, Tasks $tasks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tasks $tasks)
+    {
+        //
+    }
+}
